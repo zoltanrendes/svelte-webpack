740ea71f42080e720775019b36ce27784aaf6760
Zoltan Rendes <rendes.zoltan@gmail.com>
2020-04-17 21:44:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# svelte app
+
+A simple template for [Svelte](https://svelte.dev) apps.
+
+# install
+yarn && yarn dev
---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+	export let countStore;
+</script>
+
+<style>
+	.intro {
+		--intro-color: rgb(0, 129,251);
+
+		position: absolute;
+		text-align: center;
+		color: var(--intro-color);
+		border: 1px solid var(--intro-color);
+		border-radius: 4px;
+		padding: 4px;
+		margin: 4px;
+		top: 0;
+		left: 0;
+		right: 0;
+	}
+</style>
+
+<div class="container-fluid">
+	<div class="jumbotron intro">
+		<h2>Hello {name}!</h2>
+		<h3>{$countStore}</h3>
+		<button class="btn btn-lg btn-primary" on:click={countStore.increment}>+</button>
+		<button class="btn btn-lg btn-primary" on:click={countStore.decrement}>-</button>
+		<button class="btn btn-lg btn-primary" on:click={countStore.reset}>reset</button>
+	</div>
+</div>
